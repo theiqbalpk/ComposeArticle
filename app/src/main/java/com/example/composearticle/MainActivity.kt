5cc7207c0ddf02c057aece16279ffde468e61ee4
iqbal Ahmad <theiqbalpk@gmail.com>
2024-01-27 20:01:10 +0500

    code style change

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,23 +3,23 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composearticle.ui.theme.ComposeArticleTheme
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ComposeArticleApp()
+                    ComposeArticle()
                 }
             }
         }
@@ -39,47 +39,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposeArticleApp() {
-    ArticleCard(
-        title = stringResource(R.string.jetpack_compose_tutorial),
-        shortDescription = stringResource(R.string.compose_short_desc),
-        longDescription = stringResource(R.string.compose_long_desc),
-        imagePainter = painterResource(R.drawable.bg_compose_background)
-    )
-}
-
-@Composable
 fun ArticleCard(
+    imagePainter: Painter,
     title: String,
     shortDescription: String,
     longDescription: String,
-    imagePainter: Painter,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
-        Image(painter = imagePainter, contentDescription = null)
+        Image(
+            painter = imagePainter,
+            contentDescription = null
+        )
         Text(
             text = title,
-            modifier = Modifier.padding(16.dp),
-            fontSize = 24.sp
-        )
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+            )
         Text(
             text = shortDescription,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-            textAlign = TextAlign.Justify
-        )
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+            )
         Text(
             text = longDescription,
-            modifier = Modifier.padding(16.dp),
-            textAlign = TextAlign.Justify
-        )
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+            )
     }
+}
+
+@Composable
+fun ComposeArticle() {
+    ArticleCard(
+        imagePainter = painterResource(id = R.drawable.bg_compose_background),
+        title = stringResource(R.string.title_jetpack_compose_tutorial),
+        shortDescription = stringResource(R.string.compose_short_desc),
+        longDescription = stringResource(R.string.compose_long_desc)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
-        ComposeArticleApp()
+        ComposeArticle()
     }
 }
